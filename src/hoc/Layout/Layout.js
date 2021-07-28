1d2fab7bdcb6105ca0d19efe7cb544c3ceb15b3a
anik95 <mustadirmahmood840@gmail.com>
2021-07-28 11:58:10 +0600

    first commit

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,40 @@
+import React, { Component, Fragment } from 'react'
+
+import classes from './Layout.module.css'
+import Toolbar from './../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+  
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => ({
+      showSideDrawer: !prevState.showSideDrawer
+    }))
+  }
+  
+  render() {
+    return (
+      <Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <div>Toolbar,Side drawer, Backdrop</div>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Fragment>
+    )
+  }
+}
+
+
+export default Layout
